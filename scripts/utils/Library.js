f32cfd75fe2274ffddc7a235fd62ad2e6d253903
Jakub Löffelmann <jakubloffelmann@gmail.com>
2017-09-10 21:46:47 +0200

    ESlint fixing

--- scripts/utils/Library.js
+++ scripts/utils/Library.js
@@ -4,7 +4,7 @@
  * @return {String} actual mysql TIMESTAMP
  */
 exports.getMySQLTime = function () {
-    return new Date().toISOString().slice(0, 19).replace('T', ' ');
+    return new Date().toISOString().slice(0, 19).replace("T", " ");
 };
 
 /**
