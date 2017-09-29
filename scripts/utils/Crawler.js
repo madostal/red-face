84487348eab9bb27d41530b5e58d4946c0202102
Jakub Löffelmann <jakubloffelmann@gmail.com>
2017-09-29 20:52:20 +0200

    Update webDriver, added crawler

---
+++ scripts/utils/Crawler.js
@@ -0,0 +1,74 @@
+var webDriver = require('./WebDriver');
+
+module.exports = class Crawler {
+
+    constructor(home) {
+        this.home = home;
+        this.webDriver = new webDriver();
+        this.urls = new Array();
+        // visited - url - writable elemets
+        this.urls.push([false, home, false]);
+    }
+
+    _wasCrawled(input) {
+        for (var i = 0; i < this.urls.length; i++) {
+            if (this.urls[i][1] === input) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    _getUnvisited() {
+        for (var i = 0; i < this.urls.length; i++) {
+            if (!this.urls[i][0]) {
+                this.urls[i][0] = true;
+                return this.urls[i];
+            }
+        }
+        return null;
+    }
+
+    _setHasWritable(url) {
+        for (var i = 0; i < this.urls.length; i++) {
+            if (this.urls[i][1] === url) {
+                this.urls[i][2] = true;
+            }
+        }
+    }
+
+    crawle() {
+        var url = this._getUnvisited();
+        while (url !== null) {
+
+            this.webDriver.goTo(url[1]);
+
+            //check if page has some input, text area etc..
+            if (this.webDriver.hasWritableElements()) {
+                this._setHasWritable(url[1]);
+            }
+
+            var lastLinkst = this.webDriver.extractAllLinks();
+
+            for (var i = 0; i < lastLinkst.length; i++) {
+                var value = lastLinkst[i];
+                if (!this._wasCrawled(value) && (value.startsWith(this.home) && value !== url)) {
+                    this.urls.push([false, value, false]);
+                }
+            }
+
+            url = this._getUnvisited();
+        }
+
+        for (var i = 0; i < this.urls.length; i++) {
+            console.log(this.urls[i][1] + ", " + this.urls[i][2]);
+        }
+
+        this._shutDown();
+    }
+
+    _shutDown() {
+        this.webDriver.closeDriver();
+    }
+
+}
--- scripts/utils/WebDriver.js
+++ scripts/utils/WebDriver.js
@@ -1,21 +1,24 @@
 require('chromedriver');
+var sleep = require('system-sleep');
 
-var webdriver = require('selenium-webdriver'), 
+var webdriver = require('selenium-webdriver'),
 	By = webdriver.By,
-    until = webdriver.until;
+	until = webdriver.until;
 
-		const CHROME_OPTRIONS = {
-		//'args': ['--test-type', '--start-maximized', '--headless']
-		'args': ['--test-type']
-	};
-	
-module.exports =  class WebDriver {
-	
+const CHROME_OPTRIONS = {
+	//'args': ['--test-type', '--start-maximized', '--headless']
+	'args': ['--test-type']
+};
+
+const GUARD_LOCK_TIME_MS = 1000;
+
+module.exports = class WebDriver {
+
 	constructor() {
 		this.driver = null;
 		this._init();
 	}
-	
+
 	_init() {
 		var chromeCapabilities = webdriver.Capabilities.chrome();
 		this.driver = new webdriver.Builder()
@@ -23,23 +26,61 @@ module.exports =  class WebDriver {
 			.withCapabilities(chromeCapabilities)
 			.build();
 	}
-	
+
 	screenShot(path) {
-		this.driver.takeScreenshot()
-			.then(
-				function(image, err) {
-					require('fs').writeFile('out.png', image, 'base64', function(err) {
-						console.log(err);
-					});
-				}
-			);
+		this._simplePromiseGuard(() => this.driver.takeScreenshot()
+			.then(function (image, err) {
+				require('fs').writeFile(path, image, 'base64', function (err) {
+					//
+				});
+			}));
 	}
-	
+
 	goTo(url) {
-		console.log(this.driver.get(url).then().extract().response());
+		this._simplePromiseGuard(() => this.driver.get(url));
 	}
-	
+
 	closeDriver() {
-		this.driver.quit();
+		this._simplePromiseGuard(() => this.driver.quit());
+	}
+
+	extractAllLinks() {
+		return this._simplePromiseGuard(() =>
+			this.driver.executeAsyncScript(function () {
+				var callback = arguments[arguments.length - 1];
+
+				var data = [], l = document.links;
+				for (var i = 0; i < l.length; i++) {
+					data.push(l[i].href);
+				}
+				callback(data);
+			})
+		);
+	}
+
+	hasWritableElements() {
+		return this._simplePromiseGuard(() =>
+			this.driver.executeAsyncScript(function () {
+				var callback = arguments[arguments.length - 1];
+
+				var selector = 'input[type="text"], input[type="password"], textarea';
+				callback(document.querySelectorAll(selector.length > 0));
+			})
+		);
+	}
+
+	_simplePromiseGuard(fn) {
+		var returnData;
+		var state = false;
+		fn()
+			.then((data) => {
+				returnData = data;
+				state = true;
+			});
+
+		while (!state) {
+			sleep(GUARD_LOCK_TIME_MS);
+		}
+		return returnData;
 	}
 }
