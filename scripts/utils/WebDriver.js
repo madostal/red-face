db020622c26018202ed1362b6c280eaa0f8ef8f5
Jakub Löffelmann <jakubloffelmann@gmail.com>
2017-10-01 23:06:32 +0200

    Prepare webdriver for bruteforcetask

--- scripts/utils/WebDriver.js
+++ scripts/utils/WebDriver.js
@@ -5,9 +5,14 @@ var webdriver = require('selenium-webdriver'),
 	By = webdriver.By,
 	until = webdriver.until;
 
-const CHROME_OPTRIONS = {
-	//'args': ['--test-type', '--start-maximized', '--headless']
-	'args': ['--test-type']
+const CHROME_OPTIONS = {
+	// 'args': ['--test-type', '--start-maximized', '--headless']
+	'args': [
+		'--test-type', 'disable-web-security'
+	],
+	"prefs": {
+		"profile.managed_default_content_settings.images": 2
+	}
 };
 
 const GUARD_LOCK_TIME_MS = 1000;
@@ -21,6 +26,8 @@ module.exports = class WebDriver {
 
 	_init() {
 		var chromeCapabilities = webdriver.Capabilities.chrome();
+		chromeCapabilities.set('chromeOptions', CHROME_OPTIONS);
+
 		this.driver = new webdriver.Builder()
 			.forBrowser('chrome')
 			.withCapabilities(chromeCapabilities)
@@ -36,8 +43,12 @@ module.exports = class WebDriver {
 			}));
 	}
 
-	goTo(url) {
-		this._simplePromiseGuard(() => this.driver.get(url));
+	goTo(url, guardTime = GUARD_LOCK_TIME_MS) {
+		this._simplePromiseGuard(() => this.driver.get(url), guardTime);
+	}
+
+	goToAsynch(url) {
+		this.driver.get(url);
 	}
 
 	closeDriver() {
@@ -69,7 +80,50 @@ module.exports = class WebDriver {
 		);
 	}
 
-	_simplePromiseGuard(fn) {
+	goBack(guardTime = GUARD_LOCK_TIME_MS) {
+		return this._simplePromiseGuard(() =>
+			this.driver.executeAsyncScript(function () {
+				var callback = arguments[arguments.length - 1];
+				history.back();
+				callback(null);
+			}), guardTime);
+	}
+
+	doLogin(login, psw, xpForm, guardTime = GUARD_LOCK_TIME_MS) {
+		var data = [login, psw, xpForm];
+		return this._simplePromiseGuard(() =>
+			this.driver.executeAsyncScript(function (data) {
+				var callback = arguments[arguments.length - 1];
+
+				console.log(data);
+				function getElementByXpath(path) {
+					return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+				}
+
+				var loginForm = getElementByXpath(data[2]);
+
+				var userName = ["(", [data[2], "//input"].join(""), ")[1]"].join("");
+				var psw = ["(", [data[2], "//input"].join(""), ")[2]"].join("");
+
+				getElementByXpath(userName).value = data[0];
+				getElementByXpath(psw).value = data[1];
+				loginForm.submit();
+
+				callback(null);
+			}, data), guardTime);
+	}
+
+	getDocumentText(guardTime = GUARD_LOCK_TIME_MS) {
+		return this._simplePromiseGuard(() =>
+			this.driver.executeAsyncScript(function () {
+				var callback = arguments[arguments.length - 1];
+
+				callback(document.body.textContent);
+			}), guardTime);
+	}
+
+	_simplePromiseGuard(fn, sleepTime = GUARD_LOCK_TIME_MS) {
+		console.log(sleepTime);
 		var returnData;
 		var state = false;
 		fn()
@@ -79,7 +133,7 @@ module.exports = class WebDriver {
 			});
 
 		while (!state) {
-			sleep(GUARD_LOCK_TIME_MS);
+			sleep(sleepTime);
 		}
 		return returnData;
 	}
